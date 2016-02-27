e8ef409ab31bd6a1f7d3725b4a0589fe531a6ca3
Alejandra <stephaniacernas@gmail.com>
2016-02-27 12:23:17 -0600

    Adds edit functionality in zoos

--- app/controllers/zoos_controller.rb
+++ app/controllers/zoos_controller.rb
@@ -1,6 +1,31 @@
 class ZoosController < ApplicationController
+
+  before_filter :find_zoo, only: [:edit, :update]
+
   def index
     @zoos = Zoo.all
   end
 
+  def edit
+  end
+
+  def update
+    if @zoo.update(zoo_params)
+      flash[:message] = t('zoo.update_succesfully')
+      redirect_to zoos_path
+    else
+      flash[:message] = t('zoo.update_error')
+      render :edit
+    end
+  end
+
+  private
+
+  def zoo_params
+    params.require(:zoo).permit(:name)
+  end
+
+  def find_zoo
+    @zoo = Zoo.find(params[:id])
+  end
 end
