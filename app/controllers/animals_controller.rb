fa88b162c6dbd09ab6d1dce87e22c76cafa6a6dd
Alejandra <stephaniacernas@gmail.com>
2016-02-27 16:14:35 -0600

    Adds create animal functionality and validates model fields

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,13 +1,36 @@
 class AnimalsController < ApplicationController
 
   before_filter :find_animal, only: [:edit, :update, :destroy, :show]
-  before_filter :find_zoo, only: [:index]
+  before_filter :find_zoo
 
   def index
     @animals = @zoo.animals
   end
 
+  def new
+    @animal = @zoo.animals.build
+  end
+
+  def create
+    @animal = @zoo.animals.build(animal_params)
+    if @animal.save
+      flash[:message] = t('zoo.create_succesfully')
+      redirect_to zoo_animals_path
+    else
+      flash[:message] = t('zoo.create_error')
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+
   private
+  def animal_params
+    params.require(:animal).permit(:name, :specie_id)
+  end
 
   def find_zoo
     @zoo = Zoo.find(params[:zoo_id])
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,7 @@
 class Animal < ActiveRecord::Base
   belongs_to :zoo
   belongs_to :specie
+
+  validates :name, presence: true
+  validates :specie, presence: true
 end
