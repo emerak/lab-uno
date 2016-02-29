95dca0efa81ba58b08dd386f93fa94f26f354a3f
Alejandra <stephaniacernas@gmail.com>
2016-02-28 22:32:06 -0600

    Fixes animal display

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,7 +4,7 @@ class AnimalsController < ApplicationController
   before_filter :find_zoo, only: [:index, :new, :edit, :create]
 
   def index
-    @animals = Animal.all.order(:name).page params[:page]
+    @animals = @zoo.animals.order(:name).page params[:page]
   end
 
   def new
